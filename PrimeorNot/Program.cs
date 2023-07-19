4c72a4ce2ec98c98c6416a78acc541c37712d1dd
M.Haris Khan <hkhan@cedrus.com>
2023-07-19 12:31:17 +0500

    Feat;PrimeorNot

---
+++ PrimeorNot/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeorNot
+{
+
+    class details
+    {
+        public int num;
+
+        public void getdetails()
+        {
+            Console.Write("Enter a Number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+
+
+        }
+    }
+    class prime : details
+    {
+        int f = 0;
+        public void findprime()
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    f++;
+                }
+            }
+            if (f == 2)
+            {
+                Console.WriteLine("Number is Prime.");
+            }
+            else
+            {
+                Console.WriteLine("Number is Not Prime.");
+            }
+            Console.ReadLine();
+
+
+
+        }
+
+    }
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            var prime = new prime();
+            prime.getdetails();
+            prime.findprime();
+
+        }
+    }
+
+}
